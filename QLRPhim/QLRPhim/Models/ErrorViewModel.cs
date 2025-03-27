2e2db936ccebdb56090faf560e6864d9825b9218
Huyhenz <tudomb2004@gmail.com>
2025-03-27 23:57:29 +0700

    Xoa

--- QLRPhim/QLRPhim/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QLRPhim.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
