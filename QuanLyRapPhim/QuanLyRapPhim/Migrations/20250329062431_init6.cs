39ce96809b3d06ff007a4fc85f7be05d3990c4f4
Huyhenz <tudomb2004@gmail.com>
2025-03-29 13:27:31 +0700

    Sửa lại add-migrationinit6

---
+++ QuanLyRapPhim/QuanLyRapPhim/Migrations/20250329062431_init6.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyRapPhim.Migrations
+{
+    /// <inheritdoc />
+    public partial class init6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Showtimes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Showtimes_RoomId",
+                table: "Showtimes",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+            name: "FK_Showtimes_Rooms_RoomId",
+            table: "Showtimes",
+            column: "RoomId",
+            principalTable: "Rooms",
+            principalColumn: "RoomId",
+            onDelete: ReferentialAction.Restrict);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Showtimes_Rooms_RoomId",
+                table: "Showtimes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Showtimes_RoomId",
+                table: "Showtimes");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Showtimes");
+        }
+    }
+}
--- QuanLyRapPhim/QuanLyRapPhim/Models/Showtime.cs
+++ QuanLyRapPhim/QuanLyRapPhim/Models/Showtime.cs
@@ -12,7 +12,8 @@ namespace QuanLyRapPhim.Models
         public string Poster { get; set; }
         public string StartTime { get; set; }
         public DateTime Date { get; set; }
-
+        public int RoomId { get; set; }
+        public Room? Room { get; set; }
         public Movie? Movie { get; set; }
         public ICollection<Booking>? Bookings { get; set; }
     }
